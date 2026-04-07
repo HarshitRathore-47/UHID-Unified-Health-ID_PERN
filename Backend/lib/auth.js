85167b91cd49426faf0f69269f5ee24bd65cf105
Harshit Rathore <harshitrath9990@gmail.com>
2026-04-07 15:01:39 +0530

    Transporter config

--- Backend/lib/auth.js
+++ Backend/lib/auth.js
@@ -27,6 +27,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
+  family: 4,
   tls: {
     rejectUnauthorized: false // Dono jagah connection stable rakhega
   }
